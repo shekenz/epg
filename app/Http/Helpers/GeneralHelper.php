813ccfb67d6ceca4dad4e540db1e183d6ec83779
shekenz <2657910+shekenz@users.noreply.github.com>
2021-10-29 07:49:14 +0200

    Update : protected helpers with function_exists()

--- app/Http/Helpers/GeneralHelper.php
+++ app/Http/Helpers/GeneralHelper.php
@@ -1,18 +1,24 @@
 <?php
 
-function findStopPrice(int $totalWeight, float $defaultPrice, Illuminate\Support\Collection $priceList) {
-	$priceList->each( function($priceStop) use ($totalWeight, &$defaultPrice) {
-		if($totalWeight >= $priceStop->weight) {
-			$defaultPrice = $priceStop->price;
-		}
-	});
-	return $defaultPrice;
+if(!function_exists('findStopPrice')) {
+	function findStopPrice(int $totalWeight, float $defaultPrice, Illuminate\Support\Collection $priceList) {
+		$priceList->each( function($priceStop) use ($totalWeight, &$defaultPrice) {
+			if($totalWeight >= $priceStop->weight) {
+				$defaultPrice = $priceStop->price;
+			}
+		});
+		return $defaultPrice;
+	}
 }
 
-function mb_ucfirst($string) {
-	return mb_strtoupper(mb_substr($string, 0, 1)).mb_substr($string, 1);
+if(!function_exists('mb_ucfirst')) {
+	function mb_ucfirst($string) {
+		return mb_strtoupper(mb_substr($string, 0, 1)).mb_substr($string, 1);
+	}
 }
 
-function ___($string) {
-	return mb_ucfirst(__($string));
+if(!function_exists('___')) {
+	function ___($string) {
+		return mb_ucfirst(__($string));
+	}
 }
