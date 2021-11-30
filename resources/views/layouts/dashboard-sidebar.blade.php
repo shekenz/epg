2a81d4fbc9a77078759beef89a9908981ef84cd3
shekenz <2657910+shekenz@users.noreply.github.com>
2021-11-30 10:33:38 +0100

    Update : Translation and missing item

--- resources/views/layouts/dashboard-sidebar.blade.php
+++ resources/views/layouts/dashboard-sidebar.blade.php
@@ -1,16 +1,18 @@
 <nav id="left-bar" class="side-bar">
 	<a class="side-bar-item @if(request()->routeIs('books')) {{ 'active' }} @endif" href="{{ route('books') }}">
-		<x-tabler-book />&nbsp;Livres
+		<x-tabler-book />&nbsp;{{ ___('books') }}
 	</a>
 	<a class="side-bar-item @if(request()->routeIs('media')) {{ 'active' }} @endif" href="{{ route('media') }}">
-		<x-tabler-photo />&nbsp;Média
+		<x-tabler-photo />&nbsp;{{ ___('media') }}
 	</a>
 	<a class="side-bar-item @if(request()->routeIs('orders')) {{ 'active' }} @endif" href="{{ route('orders') }}">
-		<x-tabler-receipt />&nbsp;Commandes</a>
+		<x-tabler-receipt />&nbsp;{{ ___('orders') }}</a>
+	<a class="side-bar-item @if(request()->routeIs('clients')) {{ 'active' }} @endif" href="{{ route('clients') }}">
+		<x-tabler-mood-smile />&nbsp;{{ ___('clients') }}</a>
 	<a class="side-bar-item @if(request()->routeIs('users')) {{ 'active' }} @endif" href="{{ route('users') }}">
-		<x-tabler-users />&nbsp;Utilisateurs</a>
+		<x-tabler-users />&nbsp;{{ ___('users') }}</a>
 	<a class="side-bar-item @if(request()->routeIs('settings')) {{ 'active' }} @endif" href="{{ route('settings') }}">
-		<x-tabler-settings />&nbsp;Paramètres
+		<x-tabler-settings />&nbsp;{{ ___('settings') }}
 	</a>
 	<div class="flex-grow"></div>
 	<span class="text-gray-400 dark:text-gray-500 self-center m-2 text-sm">Created by <a class="underline" href="https://github.com/shekenz">Shekenz</a></span>
