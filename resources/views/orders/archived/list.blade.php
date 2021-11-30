20f39ae32f77218aa2bf56ba5a13c58443b55057
shekenz <2657910+shekenz@users.noreply.github.com>
2021-11-30 22:53:24 +0100

    Update : Orders section

--- resources/views/orders/archived/list.blade.php
+++ resources/views/orders/archived/list.blade.php
@@ -2,51 +2,51 @@
 	<x-slot name="title">
 			{{ ___('archived orders') }}
 	</x-slot>
-
-	<x-slot name="leftControls">
-		<a href="{{ route('orders') }}" class="mini-button"><x-tabler-chevron-left /></a>
-	</x-slot>
 	
-	<form id="orders-selection" method="POST">
-		@csrf
-		<table id="orders-table" class="app-table">
-			<thead>
-				<td>{{ ___('order') }}</td>
-				<td>{{ ___('client') }}</td>
-				<td>{{ ___('client email') }}</td>
-				<td>{{ ___('created at') }}</td>
-				<td>{{ ___('archived at') }}</td>
-				<td>{{ ___('version') }}</td>
-			</thead>
-			<tbody id="order-rows">
-				@foreach($archivedOrders as $archiveOrder)
-				<tr>
-					<td><a class="underline text-gray-500 hover:text-inherit" href="{{ route('archive.display', $archiveOrder->id) }}">{{ $archiveOrder->order_id }}</a></td>
-					<td>{{ $archiveOrder->full_name }}</td>
-					<td>{{ $archiveOrder->email_address }}</td>
-					<td>{{ $archiveOrder->created_at }}</td>
-					<td>{{ $archiveOrder->archived_at }}</td>
-					<td>{{ $archiveOrder->version }}</td>
-				</tr>
-				@endforeach
-				{{--
-				<tr>
-					<td colspan="8"><img class="m-auto my-8" src="{{ asset('img/loader2.gif') }}" alt="loader animation"></td>
-				</tr>
-				--}}
-			</tbody>
-		</table>
-	</form>
+	<x-section :return="route('orders')" :title="___('archived orders')" class="full">
+	
+		<form id="orders-selection" method="POST">
+			@csrf
+			<table id="orders-table" class="app-table">
+				<thead>
+					<td>{{ ___('order') }}</td>
+					<td>{{ ___('client') }}</td>
+					<td>{{ ___('client email') }}</td>
+					<td>{{ ___('created at') }}</td>
+					<td>{{ ___('archived at') }}</td>
+					<td>{{ ___('version') }}</td>
+				</thead>
+				<tbody id="order-rows">
+					@foreach($archivedOrders as $archiveOrder)
+					<tr>
+						<td><a class="underline text-gray-500 hover:text-inherit" href="{{ route('archive.display', $archiveOrder->id) }}">{{ $archiveOrder->order_id }}</a></td>
+						<td>{{ $archiveOrder->full_name }}</td>
+						<td>{{ $archiveOrder->email_address }}</td>
+						<td>{{ $archiveOrder->created_at }}</td>
+						<td>{{ $archiveOrder->archived_at }}</td>
+						<td>{{ $archiveOrder->version }}</td>
+					</tr>
+					@endforeach
+					{{--
+					<tr>
+						<td colspan="8"><img class="m-auto my-8" src="{{ asset('img/loader2.gif') }}" alt="loader animation"></td>
+					</tr>
+					--}}
+				</tbody>
+			</table>
+		</form>
 
-	<div id="no-result" class="hidden text-center text-xl text-gray-400 my-8">
-		{{ __('No result found') }}
-	</div>
+		<div id="no-result" class="hidden text-center text-xl text-gray-400 my-8">
+			{{ __('No result found') }}
+		</div>
 
-	<x-tabler-recycle class="hidden" id="recycle-blueprint"/>
-	<x-tabler-trash class="hidden" id="trash-blueprint"/>
-	{{-- Used to be a forkilft icon, hence the id (used in order-list.js) --}}
-	<x-tabler-clock class="hidden" id="forklift-blueprint"/>
-	<x-tabler-truck-delivery class="hidden" id="shipped-blueprint"/>
-	<x-tabler-archive class="hidden" id="archive-blueprint"/>
+		<x-tabler-recycle class="hidden" id="recycle-blueprint"/>
+		<x-tabler-trash class="hidden" id="trash-blueprint"/>
+		{{-- Used to be a forkilft icon, hence the id (used in order-list.js) --}}
+		<x-tabler-clock class="hidden" id="forklift-blueprint"/>
+		<x-tabler-truck-delivery class="hidden" id="shipped-blueprint"/>
+		<x-tabler-archive class="hidden" id="archive-blueprint"/>
+
+	</x-section>
 
 </x-app-layout>
--- resources/views/orders/list.blade.php
+++ resources/views/orders/list.blade.php
@@ -24,103 +24,107 @@
 	</x-slot>
 	@endif
 
-	<div class="flex justify-between mb-3">
-		<div>
-			@php $maxDate = \Carbon\Carbon::now()->toDateString(); @endphp
-			<label for="filter">{{ ___('filter') }}</label>
-			<select class="input-inline" id="filter" placeholder="Filter">
-				<option selected value="all">{{ ___('none') }}</option>
-				<option value="book">{{ ___('book') }}</option>
-				<option value="order">{{ ___('order ID') }}</option>
-				<option value="name">{{ ___('client name') }}</option>
-				<option value="email">{{ ___('email') }}</option>
-				<option value="status">{{ ___('status') }}</option>
-				<option value="coupon">{{ ___('coupon') }}</option>
-				<option value="shipping">{{ ___('shipping method') }}</option>
-			</select>
-			<label for="filter-data">{{ ___('with') }}</label>
-			<input class="input-inline" id="filter-data-text" type="text" disabled="true">
-			{{-- Books filter option --}}
-			<select class="input-inline hidden max-w-[16rem]" id="filter-data-book">
-				<option value=""></option>
-				@foreach ($books as $book)
-					<option value="{{ $book->id }}">{{ $book->bookInfo['title'].' | '.$book->label }}@if($book->trashed()) ({{ ___('archived') }})@endif</option>
-				@endforeach
-			</select>
-			{{-- Statut filter option --}}
-			<select class="input-inline hidden" id="filter-data-status">
-				<option value="FAILED">{{ __('paypal.status.FAILED') }}</option>
-				<option value="CREATED">{{ __('paypal.status.CREATED') }}</option>
-				<option value="COMPLETED" selected="selected">{{ __('paypal.status.COMPLETED') }}</option>
-				<option value="SHIPPED">{{ __('paypal.status.SHIPPED') }}</option>
-			</select>
-			{{-- Coupon filter option --}}
-			<select class="input-inline hidden" id="filter-data-coupon">
-				<option value="">{{ ___('none') }}</option>
-				@foreach ($coupons as $coupon)
-					<option value="{{ $coupon->id }}">{{ $coupon->label }}@if($coupon->trashed()) ({{ ___('trashed') }})@endif</option>
-				@endforeach
-			</select>
-			{{-- Shipping method filter option --}}
-			<select class="input-inline hidden" id="filter-data-shipping">
-				@foreach ($shippingMethods as $shippingMethod)
-					<option value="{{ $shippingMethod->id }}">{{ $shippingMethod->label }}@if($shippingMethod->trashed()) ({{ ___('trashed') }})@endif</option>
-				@endforeach
-			</select>
-			{{-- Datefilter option --}}
-			<label for="start-date">{{ __('from') }}</label>
-			<input class="input-inline" id="start-date" type="date" value="{{ \Carbon\Carbon::now()->subYear(1)->toDateString()}}" max="{{ $maxDate }}">
-			<label for="end-date">{{ __('to') }}</label>
-			<input class="input-inline" id="end-date" type="date" value="{{ $maxDate }}" max="{{ $maxDate }}">
-			{{-- Pre-order filter option --}}
-			<input class="ml-2" id="preorder" type="checkbox"><label for="preorder" class="label-shared"> {{ ___('pre-orders') }}</label>
-			<img id="loader" class="hidden ml-2 w-6 h-6 inline-block" src="{{ asset('img/loader2.gif')}}">
-			</select>
-		</div>
-	</div>
-	<div class="flex items-end border-t pt-1">
-		<x-tabler-corner-left-down class="ml-2 inline-block" />
-		<div class="mb-2">
-			@if(request()->routeIs('orders.hidden'))
-			<input id="hide" class="button-small cursor-pointer action" type="button" data-action="{{ route('orders.unhide') }}" value="{{ ___('unhide') }}">
-			@else
-			<input id="hide" class="button-small cursor-pointer action" type="button" data-action="{{ route('orders.hide') }}" value="{{ ___('hide') }}">
-			@endif
-			<input id="csv" class="button-small cursor-pointer action" type="button" data-action="{{ route('orders.csv') }}" value="CSV">
-			<input id="pdf" class="button-small cursor-pointer action" type="button" data-action="{{ route('orders.labelsPreview') }}" value="PDF">
-		</div>
-	</div>
-	
-	<form id="orders-selection" method="POST">
-		@csrf
-		<table id="orders-table" class="app-table">
-			<thead>
-				<td><input type="checkbox" id="checkall" title="{{ ___('select all') }}"></td>
-				<td>{{ ___('order') }}</td>
-				<td>{{ ___('client') }}</td>
-				<td>{{ ___('email') }}</td>
-				<td>{{ ___('pre') }}</td>
-				<td>{{ ___('status') }}</td>
-				<td>{{ ___('created at') }}</td>
-				<td>{{ ___('tools') }}</td>
-			</thead>
-			<tbody id="order-rows">
-				<tr>
-					<td colspan="8"><img class="m-auto my-8" src="{{ asset('img/loader_medium.svg') }}" alt="loader animation"></td>
-				</tr>
-			</tbody>
-		</table>
-	</form>
+	<x-section :title="___('archived orders')" class="full">
 
-	<div id="no-result" class="hidden text-center text-xl text-gray-400 my-8">
-		{{ __('No result found') }}
-	</div>
+		<div class="flex justify-between mb-3">
+			<div>
+				@php $maxDate = \Carbon\Carbon::now()->toDateString(); @endphp
+				<label for="filter">{{ ___('filter') }}</label>
+				<select class="input-inline" id="filter" placeholder="Filter">
+					<option selected value="all">{{ ___('none') }}</option>
+					<option value="book">{{ ___('book') }}</option>
+					<option value="order">{{ ___('order ID') }}</option>
+					<option value="name">{{ ___('client name') }}</option>
+					<option value="email">{{ ___('email') }}</option>
+					<option value="status">{{ ___('status') }}</option>
+					<option value="coupon">{{ ___('coupon') }}</option>
+					<option value="shipping">{{ ___('shipping method') }}</option>
+				</select>
+				<label for="filter-data">{{ ___('with') }}</label>
+				<input class="input-inline" id="filter-data-text" type="text" disabled="true">
+				{{-- Books filter option --}}
+				<select class="input-inline hidden max-w-[16rem]" id="filter-data-book">
+					<option value=""></option>
+					@foreach ($books as $book)
+						<option value="{{ $book->id }}">{{ $book->bookInfo['title'].' | '.$book->label }}@if($book->trashed()) ({{ ___('archived') }})@endif</option>
+					@endforeach
+				</select>
+				{{-- Statut filter option --}}
+				<select class="input-inline hidden" id="filter-data-status">
+					<option value="FAILED">{{ __('paypal.status.FAILED') }}</option>
+					<option value="CREATED">{{ __('paypal.status.CREATED') }}</option>
+					<option value="COMPLETED" selected="selected">{{ __('paypal.status.COMPLETED') }}</option>
+					<option value="SHIPPED">{{ __('paypal.status.SHIPPED') }}</option>
+				</select>
+				{{-- Coupon filter option --}}
+				<select class="input-inline hidden" id="filter-data-coupon">
+					<option value="">{{ ___('none') }}</option>
+					@foreach ($coupons as $coupon)
+						<option value="{{ $coupon->id }}">{{ $coupon->label }}@if($coupon->trashed()) ({{ ___('trashed') }})@endif</option>
+					@endforeach
+				</select>
+				{{-- Shipping method filter option --}}
+				<select class="input-inline hidden" id="filter-data-shipping">
+					@foreach ($shippingMethods as $shippingMethod)
+						<option value="{{ $shippingMethod->id }}">{{ $shippingMethod->label }}@if($shippingMethod->trashed()) ({{ ___('trashed') }})@endif</option>
+					@endforeach
+				</select>
+				{{-- Datefilter option --}}
+				<label for="start-date">{{ __('from') }}</label>
+				<input class="input-inline" id="start-date" type="date" value="{{ \Carbon\Carbon::now()->subYear(1)->toDateString()}}" max="{{ $maxDate }}">
+				<label for="end-date">{{ __('to') }}</label>
+				<input class="input-inline" id="end-date" type="date" value="{{ $maxDate }}" max="{{ $maxDate }}">
+				{{-- Pre-order filter option --}}
+				<input class="ml-2" id="preorder" type="checkbox"><label for="preorder" class="label-shared"> {{ ___('pre-orders') }}</label>
+				<img id="loader" class="hidden ml-2 w-6 h-6 inline-block" src="{{ asset('img/loader2.gif')}}">
+				</select>
+			</div>
+		</div>
+		<div class="flex items-end border-t pt-1">
+			<x-tabler-corner-left-down class="ml-2 inline-block" />
+			<div class="mb-2">
+				@if(request()->routeIs('orders.hidden'))
+				<input id="hide" class="button compact cursor-pointer" type="button" data-action="{{ route('orders.unhide') }}" value="{{ ___('unhide') }}">
+				@else
+				<input id="hide" class="button compact cursor-pointer" type="button" data-action="{{ route('orders.hide') }}" value="{{ ___('hide') }}">
+				@endif
+				<input id="csv" class="button compact cursor-pointer" type="button" data-action="{{ route('orders.csv') }}" value="CSV">
+				<input id="pdf" class="button compact cursor-pointer" type="button" data-action="{{ route('orders.labelsPreview') }}" value="PDF">
+			</div>
+		</div>
+		
+		<form id="orders-selection" method="POST">
+			@csrf
+			<table id="orders-table" class="app-table">
+				<thead>
+					<td><input type="checkbox" id="checkall" title="{{ ___('select all') }}"></td>
+					<td>{{ ___('order') }}</td>
+					<td>{{ ___('client') }}</td>
+					<td>{{ ___('email') }}</td>
+					<td>{{ ___('pre') }}</td>
+					<td>{{ ___('status') }}</td>
+					<td>{{ ___('created at') }}</td>
+					<td>{{ ___('tools') }}</td>
+				</thead>
+				<tbody id="order-rows">
+					<tr>
+						<td colspan="8"><img class="m-auto my-8" src="{{ asset('img/loader_medium.svg') }}" alt="loader animation"></td>
+					</tr>
+				</tbody>
+			</table>
+		</form>
 
-	<x-tabler-recycle class="hidden" id="recycle-blueprint"/>
-	<x-tabler-trash class="hidden" id="trash-blueprint"/>
-	{{-- Used to be a forkilft icon, hence the id (used in order-list.js) --}}
-	<x-tabler-clock class="hidden" id="forklift-blueprint"/>
-	<x-tabler-truck-delivery class="hidden" id="shipped-blueprint"/>
-	<x-tabler-archive class="hidden" id="archive-blueprint"/>
+		<div id="no-result" class="hidden text-center text-xl text-gray-400 my-8">
+			{{ __('No result found') }}
+		</div>
+
+		<x-tabler-recycle class="hidden" id="recycle-blueprint"/>
+		<x-tabler-trash class="hidden" id="trash-blueprint"/>
+		{{-- Used to be a forkilft icon, hence the id (used in order-list.js) --}}
+		<x-tabler-clock class="hidden" id="forklift-blueprint"/>
+		<x-tabler-truck-delivery class="hidden" id="shipped-blueprint"/>
+		<x-tabler-archive class="hidden" id="archive-blueprint"/>
+
+	</x-section>
 
 </x-app-layout>
