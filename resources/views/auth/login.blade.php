03a23c52157f581a5947b8a3beeb9a6217638ce4
shekenz <2657910+shekenz@users.noreply.github.com>
2021-12-01 19:09:35 +0100

    Feature : New input component

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,66 +1,73 @@
 <x-guest-layout>
-    <x-auth-card>
+		<x-auth-card>
 
-        <!-- Session Status -->
-        <x-auth-session-status class="mb-4" :status="session('status')" />
+				<!-- Session Status -->
+				<x-auth-session-status class="mb-4" :status="session('status')" />
 
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
+				<!-- Validation Errors -->
+				{{--<x-auth-validation-errors class="mb-4" :errors="$errors" />--}}
 
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
+				<form method="POST" action="{{ route('login') }}">
+						@csrf
 
-            <!-- Email Address -->
+						<!-- Email Address -->
 			<!--
-            <div>
-                <x-label for="email" :value="__('Email')" />
+						<div>
+								<x-label for="email" :value="__('Email')" />
 
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus maxlength="256"/>
-            </div>
+								<x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus maxlength="256"/>
+						</div>
 			-->
 
 			<!-- Username -->
 			<!-- TODO autofocus on the field that has error -->
-            <div>
-                <x-label for="username" :value="___('username')" />
-                <x-input id="username" class="block mt-1 w-full" type="text" name="username" :value="old('username')" required maxlength="64"/>
-            </div>
+						<div>
+								<x-input
+									type="text"
+									name="username"
+									:label="___('username')"
+									:value="old('username')"
+									required
+									maxlength="64"
+								>@error('username'){{$message}}@enderror</x-input>
+						</div>
 
-            <!-- Password -->
-            <div class="mt-4">
-                <x-label for="password" :value="___('password')" />
+						<!-- Password -->
+						<div class="mt-4">
+								<x-input
+									type="password"
+									name="password"
+									:label="___('password')"
+									required
+									autocomplete="current-password" 
+									minlength="8"
+								>@error('password'){{$message}}@enderror</x-input>
+						</div>
 
-                <x-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="current-password" 
-								minlength="8"/>
-            </div>
+						<!-- Remember Me -->
+						<div class="block mt-4">
+								<label for="remember_me" class="inline-flex items-center">
+										<input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
+										<span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+								</label>
+						</div>
 
-            <!-- Remember Me -->
-            <div class="block mt-4">
-                <label for="remember_me" class="inline-flex items-center">
-                    <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
+						<div class="flex items-center justify-between mt-4">
+							@if (Route::has('password.request'))
+								<a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
+									{{ __('Forgot your password?') }}
+								</a>
+							@endif
 
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
+							@if( config('app.allow_register'))
+								<a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('register') }}">{{ __('Not yet registered?') }}</a>
+							@endif
 
-                <button class="button big" class="ml-3">
-                    {{ ___('login') }}
-                <button>
-            </div>
-			@if( config('app.allow_register'))
-            <div class="flex item-center mt-4 border-top justify-center">
-            	<a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('register') }}">{{ __('Not yet registered?') }}</a>
-            </div>
-			@endif
-        </form>
-    </x-auth-card>
+							<button class="button big">
+								{{ ___('login') }}
+							</button>
+						</div>
+					
+				</form>
+		</x-auth-card>
 </x-guest-layout>
---
+++ resources/views/components/input.blade.php
@@ -0,0 +1,57 @@
+@props(['label' => 'no-label', 'name', 'inline', 'wrapper-class'])
+
+<div class="
+	mb-2
+	@isset($wrapperClass)
+	{{ $wrapperClass }}
+	@endif
+">
+	<label for="{{ $name }}" class="
+		p-1
+		block
+		@if($attributes->has('disabled'))
+		text-gray-400
+		dark:text-gray-500
+		@endif
+	">{{ $label }} : </label>
+	<div class="{{ ($slot->isEmpty() || $attributes->has('disabled')) ?: 'border bg-red-400 border-red-400' }}">
+	<input
+		class="
+			px-4
+			w-full
+			dark:border-gray-600
+			dark:bg-gray-600
+			border
+			focus:ring-0
+			@if($slot->isEmpty())
+			border-background
+			focus:border-gray-400
+			dark:focus:bg-gray-500
+			dark:focus:border-gray-500
+			@else
+			border-transparent
+			focus:border-transparent
+			@endif
+			shadow-sm
+			dark:shadow-none
+			@if($attributes->has('disabled'))
+			text-gray-400
+			dark:text-gray-500
+			bg-gray-100
+			dark:bg-opacity-40
+			dark:border-transparent
+			@else
+			text-primary-dark
+			dark:text-white
+			bg-white
+			@endif
+		"
+		id="{{ $name }}"
+		name="{{ $name }}"
+		{{ $attributes }}
+	/>
+	@if($slot->isNotEmpty() && !$attributes->has('disabled'))
+		<span class="text-white px-1 italic text-sm">{{ $slot }}</span>
+	@endif
+	</div>
+</div>
