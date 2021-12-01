2e4137a672fa373b40df8bf5634337ebd85dbb2c
shekenz <2657910+shekenz@users.noreply.github.com>
2021-12-01 23:24:26 +0100

    Update : section component hodeable

--- resources/views/components/section.blade.php
+++ resources/views/components/section.blade.php
@@ -1,12 +1,16 @@
-@props(['title', 'return'])
+@props(['title', 'return', 'foldable'])
 
 <section {{ $attributes->class(['titled' => isset($title)]) }}>
 	@isset($title)
-		<h2>
+		<h2 class="flex">
 			@isset($return)
 			<a class="button icon" href="{{ $return }}"><x-tabler-arrow-left /></a>
 			@endif
 			<span class="mx-4 @isset($return) ml-2 @endif">{{ $title }}</span>
+			@isset($foldable)
+			<div class="flex-grow"></div>
+			<a class="cursor-pointer float-right block mr-2 text-primary-ultra-light hover:text-white" onclick="this.parentElement.nextElementSibling.classList.toggle('hidden')"><x-tabler-square-minus /></a>
+			@endif
 		</h2>
 	@endif
 	<div class="rounded-b-md p-4">
