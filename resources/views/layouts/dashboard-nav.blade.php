5998b7e1427039583fbbb8ccac6c0a533b91f6bd
shekenz <2657910+shekenz@users.noreply.github.com>
2021-12-01 12:49:54 +0100

    Feature : New loader component

--- resources/views/layouts/dashboard-nav.blade.php
+++ resources/views/layouts/dashboard-nav.blade.php
@@ -5,7 +5,7 @@
 		</a>
 	</div>
 	<div id="save-loader" class="flex items-center px-4 hidden">
-		<img class="w-6 h-6 mr-2" src="{{ asset('img/loader.svg') }}" alt="animated loader" />{{ ___('saving') }}...
+		<x-loader class="w-6 h-6 mr-2" />{{ ___('saving') }}...
 	</div>
 	<div class="flex-grow"></div>
 	<div id="menu-right">
