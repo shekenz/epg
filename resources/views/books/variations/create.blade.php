47fd00c59aa73c3872e60af4517eb83c6549285c
shekenz <2657910+shekenz@users.noreply.github.com>
2021-12-02 22:50:07 +0100

    Update : Use new components

--- resources/views/books/variations/create.blade.php
+++ resources/views/books/variations/create.blade.php
@@ -1,10 +1,8 @@
 <x-app-layout>
-	<x-slot name='title'>
-		{{ ___('add variation to book').' "'.$bookInfo->title.'"' }}
-	</x-slot>
 
-	<x-slot name="leftControls">
-		<a href="{{ route('books.edit', $bookInfo->id) }}" class="mini-button"><x-tabler-chevron-left /></a>
+	@php $escapedTitle = ___('add variation to book').' "'.$bookInfo->title.'"'; @endphp
+	<x-slot name='title'>
+		{{ $escapedTitle }}
 	</x-slot>
 
 	<x-slot name="scripts">
@@ -12,77 +10,60 @@
 		<script src="{{ asset('js/variations-create.js') }}" type="text/javascript" defer></script>
 	</x-slot>
 
-	<div class="m-4">
+	<x-section :title="$escapedTitle" :return="route('books.edit', $bookInfo->id)" class="full">
+
 		@if ($errors->any())
-		<div class="mb-4" :errors="$errors">
-				<div class="font-medium text-red-600">
-						{{ __('Whoops! Something went wrong.') }}
-				</div>
-
-				<ul class="mt-3 list-disc list-inside text-sm text-red-600">
-						@foreach ($errors->all() as $error)
-								<li>{{ $error }}</li>
-						@endforeach
-				</ul>
-		</div>
+			<x-warning>{{ __('app.errors.form') }}</x-warning>
 		@endif
-	</div>
 
-	<div class="m-4">
-		<form action="{{ route('variations.store', $bookInfo->id) }}" method="POST" enctype="multipart/form-data" class="grid grid-cols-4 justify-between items-stretch gap-x-4 gap-y-2 lg:m-2" autocomplete="off">
+		<form action="{{ route('variations.store', $bookInfo->id) }}" method="POST" enctype="multipart/form-data" autocomplete="off">
 		@csrf
-			<div>
-				<label for="label" class="label-shared lg:text-lg">{{ ___('label') }} :</label>
-				<input id="label" name="label" type="text" maxlength="128" class="input-shared" value="{{ old('label') }}">
-			</div>
-			<div>
-				<label for="weight" class="label-shared lg:text-lg">{{ ___('weight') }} :</label>
-				<input id="weight" name="weight" type="number" min="0" class="input-shared" value="{{ old('weight') }}">
-			</div>
-			<div>
-				<label for="stock" class="label-shared lg:text-lg">{{ ___('stock') }} :</label>
-				<input id="stock" name="stock" type="number" class="input-shared" value="{{ old('stock') }}">
-				<input id="stock-hidden" name="stock" type="hidden">
-				<div class="mt-1">
-					<input class="" id="pre-order" name="pre_order" type="checkbox" value="1" @if(old('pre_order')) {{ 'checked' }} @endif><label for="pre-order" class="text-gray-500"> {{ ___('pre-order') }}</label>
-				</div>
-			</div>
-			<div>
-				<label for="price" class="label-shared lg:text-lg">{{ ___('price') }} :</label>
-				<input id="price" name="price" type="number" min="0" step="0.01" class="input-shared" value="{{ old('price') }}">
-			</div>
 
-				<div class="col-span-4">
-					<label class="label-shared lg:text-lg">{{ ___('linked media') }} :</label>
-					<div id="media-link" class="dropzone input-mimic">
-						<div id="media-link-placeholder" class="placeholder flex m-3 justify-center items-center">
-							<span class="text-3xl text-gray-300 font-bold">{{ __('Drop media from the library here')}}.</span>
-						</div>
+			<x-separator first>{{ ___('variation data') }}</x-separator>	
+
+			<div class="flex flex-col md:grid md:grid-cols-4 md:gap-x-8">
+
+				<x-input name="label" type="text" :label="___('label')" value="{{ old('label') }}" maxlength="128">@error('label'){{$message}}@enderror</x-input>
+				<x-input name="weight" type="number" :label="___('weight').' (g)'" value="{{ old('weight') }}" min="0">@error('weight'){{$message}}@enderror</x-input>
+
+				<div>
+					<x-input name="stock" type="number" :label="___('stock')" value="{{ old('stock') }}" min="0">@error('stock'){{$message}}@enderror</x-input>
+					<div class="mt-1">
+						<input class="" id="pre-order" name="pre_order" type="checkbox" value="1" @if(old('pre_order')) {{ 'checked' }} @endif><label for="pre-order"> {{ ___('pre-order') }}</label>
 					</div>
 				</div>
+
+				<x-input name="price" type="number" :label="___('price')" value="{{ old('price') }}" min="0" step="0.01">@error('price'){{$message}}@enderror</x-input>
+
+			</div>
 				
-				<div class="col-span-4">
-					<label class="label-shared lg:text-lg">{{ ___('media library') }} :</label>
-					<div id="media-library" class="dropzone input-mimic">
-						@php $input = false; @endphp
-						@foreach($media as $medium)
-							@include('books.form-image')
-						@endforeach
-					</div>
-				</div>
+			@if( $media->isNotEmpty() )
+				<x-separator>{{ ___('media') }}</x-separator>
 
-			<div class="col-span-4">
-				<label class="label-shared lg:text-lg">{{ __('Upload and link new media') }} :</label>
-				<div class="input-mimic">
-					<input type="file" name="files[]" accept=".jpg,.jpeg,.png,.gif" multiple>
-				</div>
-			</div>
+				<x-media-dropzone id="media-link" title="{{ ___('linked media') }}">
+					<x-slot name="placeholder">{{ __('app.media.link-placeholder') }}</x-slot>
+				</x-media-dropzone>
 
-			<div class="col-span-4 mt-6 mb-4 lg:text-right">
-				<input class="button-shared" type="submit" value="{{ ___('add') }}">
-			</div>
-	
+				<x-media-dropzone id="media-library" title="{{ ___('media library') }}">
+					<x-slot name="placeholder">{{ __('app.media.library-placeholder') }}</x-slot>
+					@foreach($media as $medium)
+						<x-media-item :src="asset('storage/'.$medium->preset('thumb'))" :src2x="asset('storage/'.$medium->preset('thumb2x'))" :medium-id="$medium->id" />
+					@endforeach
+				</x-media-dropzone>
+			@endif
+
+			<x-upload :label="__('Upload and link new media')">{{ __('app.upload.limits', [
+				'max_files' => ini_get('max_file_uploads'),
+				'max_file_size' => ini_get('upload_max_filesize'),
+				'max_post_size' => ini_get('post_max_size'),
+			]) }}</x-upload>
+
+			<x-buttons bottom align="right">
+				<input class="button big cursor-pointer" type="submit" value="{{ ___('add') }}">
+			</x-buttons>
+
 		</form>
-	</div>
+	
+	</x-section>
 	
 </x-app-layout>
