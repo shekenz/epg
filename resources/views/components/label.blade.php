37de76494d50a00fb37da137ce034a3495d463b5
shekenz <2657910+shekenz@users.noreply.github.com>
2021-12-02 18:14:53 +0100

    Update : Inputs component with new label component

--- resources/views/components/label.blade.php
+++ resources/views/components/label.blade.php
@@ -1,15 +1,4 @@
-@props(['value', 'required' => false, 'format' => false])
 
-@if($required)
-<label {{ $attributes->merge(['class' => 'block font-bold text-sm text-gray-700']) }}>
-@else
-<label {{ $attributes->merge(['class' => 'block text-sm text-gray-700']) }}>
-@endif
-    {{ $value ?? $slot }}
-    @if($required)
-        <span class="font-normal text-sm">({{ __('Required') }})</span>
-    @endif
-    @if($format)
-        <span class="font-normal text-sm">({{ $format }})</span>
-    @endif
-</label>
+@php $disabled = (isset($disabled) || $attributes->has('disabled')); @endphp
+
+<label {{ $attributes }} {{ $attributes->class(['text-gray-400 dark:text-gray-500' => $disabled])->merge(['class' => 'p-1 block'])}}>{{ $slot }}</label>
