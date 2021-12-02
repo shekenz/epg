4355a1b64f739c112d682123e5d15955f5128c31
shekenz <2657910+shekenz@users.noreply.github.com>
2021-12-02 22:00:11 +0100

    Fix : Post method & single quote escaping

--- resources/views/components/post.blade.php
+++ resources/views/components/post.blade.php
@@ -1,12 +1,17 @@
-@props(['href', 'confirm', 'label', 'icon', 'title'])
+@props(['href', 'confirm', 'label', 'icon', 'title', 'method'])
 
 <form
 	action="{{ $href }}"
 	method="POST"
 	class="m-0 inline-block"
-	@isset($confirm) onsubmit="return confirm('{{ $confirm }}');" @endif
+	@isset($confirm) onsubmit="return confirm('{{ addslashes($confirm) }}')" @endif {{-- addslashes to escape single quotes --}}
 >
 	@csrf
+
+	@isset($method)
+		@method($method)
+	@endif
+
 	@isset($icon)
 		<button class="button icon" title="{{ $title }}"><x-dynamic-component :component="'tabler-'.$icon" /></button>
 	@else
