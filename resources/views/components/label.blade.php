01fa06a8165e1ebfdb99e1c5c66ce13ad1a7bfb6
shekenz <2657910+shekenz@users.noreply.github.com>
2021-12-03 12:36:06 +0100

    Fix : Attributes overriding

--- resources/views/components/label.blade.php
+++ resources/views/components/label.blade.php
@@ -1,4 +1,4 @@
 
 @php $disabled = (isset($disabled) || $attributes->has('disabled')); @endphp
 
-<label {{ $attributes }} {{ $attributes->class(['text-gray-400 dark:text-gray-500' => $disabled])->merge(['class' => 'p-1 block'])}}>{{ $slot }}</label>
+<label {{ $attributes->class(['text-gray-400 dark:text-gray-500' => $disabled])->merge(['class' => 'p-1 block'])}}>{{ $slot }}</label>
