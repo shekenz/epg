91810d9b95fcc6d695de19afb03bed1792db0d42
shekenz <2657910+shekenz@users.noreply.github.com>
2021-12-08 11:20:01 +0100

    Update : Button component diabled mode

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,6 +1,10 @@
-@props(['href', 'label', 'icon', 'title', 'warning'])
+@props(['href', 'label', 'icon', 'title', 'warning', 'disabled'])
 
-<a href="{{ $href }}" {{ $attributes->class(['icon' => isset($icon), 'warning' => isset($warning)])->merge(['class' => 'button']) }} @isset($title)title="{{ $title }}"@endif>
+<a href="{{ $href }}" {{ $attributes->class([
+		'icon' => isset($icon),
+		'warning' => isset($warning),
+		'disabled' => isset($disabled),
+	])->merge(['class' => 'button']) }} @isset($title)title="{{ $title }}"@endif>
 	@isset($icon)
 		<x-dynamic-component :component="'tabler-'.$icon" />
 	@else
