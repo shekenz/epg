cdef8359e175e589252cca83c2dc205d229a8115
shekenz <2657910+shekenz@users.noreply.github.com>
2021-12-11 17:04:19 +0100

    Update : Webview hack applied only on FB & Insta

--- resources/js/ondemand/index/add-to-cart.js
+++ resources/js/ondemand/index/add-to-cart.js
@@ -6,7 +6,8 @@ import { popFlash } from '../../shared/popup.mjs';
 const addToCartButtons = arrayByClass('add-to-cart-button');
 const translationHelper = document.getElementById('translation-helper');
 
-const flashSuccessCooledDown = popFlash('<img src="/img/frog_logo_heart.svg" alt="Frog that loves you"><span class="mx-4">'+translationHelper.dataset.addedMessage+'</span><a class="button-lg" href="/cart" target="_blank">'+translationHelper.dataset.checkoutButton+'</a>');
+// + Webview hack
+const flashSuccessCooledDown = popFlash('<img src="/img/frog_logo_heart.svg" alt="Frog that loves you"><span class="mx-4">'+translationHelper.dataset.addedMessage+'</span><a class="button-lg" href="/cart"'+(webview ? ' target="_blank"' : '')+'>'+translationHelper.dataset.checkoutButton+'</a>');
 
 const flashErrorCooledDown = message => {
 	popFlash('<img src="/img/frog_logo_warning.svg" alt="Frog that warns you"><span class="mx-4">'+message+'</span>')();
