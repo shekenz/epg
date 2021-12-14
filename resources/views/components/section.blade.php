b3cb2d8824f8cea9972de2dbb5864a010f0b0397
shekenz <2657910+shekenz@users.noreply.github.com>
2021-12-14 15:56:36 +0100

    Upate : Section component vue title compatibility

--- resources/views/components/section.blade.php
+++ resources/views/components/section.blade.php
@@ -1,12 +1,17 @@
-@props(['title', 'return', 'foldable'])
+@props(['title' => 'no-title', 'return', 'foldable'])
 
-<section {{ $attributes->class(['titled' => isset($title)]) }}>
-	@isset($title)
+@php $hasTitle = ( isset($title) || $attributes->has(':title') ); @endphp
+
+<section {{ $attributes->filter(function($val, $attr) { return !in_array($attr, [':title', '@click.prevent']); })->class(['titled' => $hasTitle]) }}>
+	@if($hasTitle)
 		<h2 class="flex">
 			@isset($return)
-			<a class="button icon" href="{{ $return }}"><x-tabler-arrow-left /></a>
+			<a class="button icon" href="{{ $return }}" @if($attributes->has('@click.prevent')) {{ $attributes->filter(function($val, $attr) { return ( $attr == '@click.prevent' ); }) }}@endif><x-tabler-arrow-left /></a>
 			@endif
-			<span class="mx-4 @isset($return) ml-2 @endif">{{ $title }}</span>
+			<span class="mx-4 @isset($return) ml-2 @endif">@php
+				// We used php tags because it was impossible to escape those damn {{ }} blade tags in that situation
+				echo ($attributes->has(':title')) ? '{{'.$attributes->get(':title').'}}' : $title ;			
+			@endphp</span>
 			@isset($foldable)
 			<div class="flex-grow"></div>
 			<a class="cursor-pointer float-right block mr-2 text-primary-ultra-light hover:text-white" onclick="this.parentElement.nextElementSibling.classList.toggle('hidden')"><x-tabler-square-minus /></a>
