8da073df66f9736def70ca222dbbc366b09e3e69
shekenz <2657910+shekenz@users.noreply.github.com>
2021-12-15 00:54:34 +0100

    Update : Button component text with icon

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,13 +1,13 @@
 @props(['href', 'label', 'icon', 'title', 'warning', 'disabled'])
 
 <a href="{{ $href }}" {{ $attributes->class([
-		'icon' => isset($icon),
+		'icon' => (isset($icon) && !isset($label)),
 		'warning' => isset($warning),
 		'disabled' => isset($disabled),
 	])->merge(['class' => 'button']) }} @isset($title)title="{{ $title }}"@endif>
-	@isset($icon)
+	@if(isset($icon) && !isset($label))
 		<x-dynamic-component :component="'tabler-'.$icon" />
 	@else
-		{{ $label }}
+		@isset($icon)<x-dynamic-component :component="'tabler-'.$icon" class="text-primary-super-light inline mr-2"/>@endif{{ $label }}
 	@endif
 </a>
