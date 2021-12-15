3afaf6aec821752d2704925038fa6ea2fa92c9bc
shekenz <2657910+shekenz@users.noreply.github.com>
2021-12-15 19:44:49 +0100

    Update : order-status component vue compatibility
    Created 2 version of the component :
    One to be rendered with vue data
    One to be rendered as a regular blade component

--- resources/views/components/captions/order-status.blade.php
+++ resources/views/components/captions/order-status.blade.php
@@ -1,13 +1,25 @@
 @props(['label', 'status'])
 
-<div class="rounded-sm text-white text-center flex justify-between py-1 px-2 gap-x-2" :class="{
-	'bg-red-500 dark:bg-red-600' : {{ $status }} == 'FAILED',
-	'bg-yellow-500' : {{ $status }} == 'CREATED',
-	'bg-blue-500 dark:bg-blue-600' : {{ $status }} == 'COMPLETED',
-	'bg-green-500 dark:bg-green-600' : {{ $status }} == 'SHIPPED',
-}">
-<x-tabler-mood-sad class="text-red-300 inline" v-if="{{ $status }} == 'FAILED'"/>
-<x-tabler-alert-triangle class="text-yellow-200 inline" v-if="{{ $status }} == 'CREATED'"/>
-<x-tabler-brand-paypal class="text-blue-300 inline" v-if="{{ $status }} == 'COMPLETED'"/>
-<x-tabler-truck-delivery class="text-green-300 inline" v-if="{{ $status }} == 'SHIPPED'"/>
-<div class="w-full text-center">@php echo '{{ $t(\'status.\'+'.$status.') }}' @endphp</div></div>
+<div {{ $attributes->class([
+	'rounded-sm text-white text-center flex justify-between py-1 px-2 gap-x-2',
+	'bg-red-500 dark:bg-red-600' => ($status == 'FAILED'),
+	'bg-yellow-500' => ($status == 'CREATED'),
+	'bg-blue-500 dark:bg-blue-600' => ($status == 'COMPLETED'),
+	'bg-green-500 dark:bg-green-600' => ($status == 'SHIPPED'),
+]) }}>
+	@switch($status)
+		@case('FAILED')
+			<x-tabler-mood-sad class="text-red-300 inline" />
+		@break
+		@case('CREATED')
+			<x-tabler-alert-triangle class="text-yellow-200 inline" />
+		@break
+		@case('COMPLETED')
+			<x-tabler-brand-paypal class="text-blue-300 inline" />
+		@break
+		@case('SHIPPED')
+			<x-tabler-truck-delivery class="text-green-300 inline" />
+		@break
+	@endswitch
+	<div class="w-full text-center">{{ ___('paypal.status.'.$status) }}</div>
+</div>
