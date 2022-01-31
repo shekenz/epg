db50d5a196bd09ebc6855eda8aacfc42d1fb2d36
shekenz <2657910+shekenz@users.noreply.github.com>
2022-01-31 14:49:40 +0100

    Refractoring : Removed unused code

--- resources/js/index.js
+++ resources/js/index.js
@@ -4,25 +4,6 @@ require('./index/menu');
 require('./shared/flash');
 //require('./shared/new-orders');
 
-//let orderUnread = document.getElementById('orderUnread');
-let orderUnread = false // Desactivated the commands notification because of performance issues.
-
-if(orderUnread) {
-	window.fetch('/api/orders/unread/count', {
-		method: 'post',
-		headers: {
-			'accept': 'application/json',
-		},
-	}).then(response => {
-		return response.json();
-	}).then(jsonResponse => {
-		if(jsonResponse.count && jsonResponse.count > 0) {
-			orderUnread.classList.remove('hidden');
-			orderUnread.appendChild(document.createTextNode(jsonResponse.count));
-		}
-	});
-}
-
 // Fixing paypal popup not working in web-view (embedded iOS app browser like Instagram or Facebook)
 window.webview = (navigator.appVersion.indexOf('AppleWebKit') !== -1 || navigator.appVersion.indexOf('Facebook') !== -1);
 if(webview) document.getElementById('cart-menu-item').setAttribute('target', '_blank');
