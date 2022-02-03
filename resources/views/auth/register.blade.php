963d7e52e85ea9d57ef6fa65186bf3c8eb292780
shekenz <2657910+shekenz@users.noreply.github.com>
2022-02-03 14:58:05 +0100

    Fix : Use new blade components

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -14,49 +14,41 @@
 
             <!-- Last name -->
             <div class="mt-4">
-                <x-label for="lastname" :value="___('last name')" :required="true" />
-                <x-input id="lastname" class="block mt-1 w-full" type="text" name="lastname" maxlength="64" :value="old('lastname')" required autofocus />
+                <x-input :label="___('last name')" name="lastname" class="block mt-1 w-full" type="text" maxlength="64" :value="old('lastname')" required autofocus />
             </div>
 
             <!-- First name -->
             <div class="mt-4">
-                <x-label for="firstname" :value="___('first name')" :required="true" />
-                <x-input id="firstname" class="block mt-1 w-full" type="text" name="firstname" maxlength="64" :value="old('firstname')" required autofocus />
+                <x-input :label="___('first name')" name="firstname" class="block mt-1 w-full" type="text" maxlength="64" :value="old('firstname')" required autofocus />
             </div>
 
             <!-- Username -->
             <div class="mt-4">
-                <x-label for="username" :value="___('username')" :required="true" />
-                <x-input id="username" class="block mt-1 w-full" type="text" name="username" maxlength="64" :value="old('username')" required />
+                <x-input :label="___('username')" name="username" class="block mt-1 w-full" type="text" maxlength="64" :value="old('username')" required />
             </div>
 
             <!-- Email Address -->
             <div class="mt-4">
-                <x-label for="email" :value="___('email')" :required="true" />
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" maxlength="256" :value="old('email')" required />
+                <x-input :label="___('email')" name="email" class="block mt-1 w-full" type="email" maxlength="256" :value="old('email')" required />
             </div>
 
             <!-- Password -->
             <div class="mt-4">
-                <x-label for="password" :value="___('password')" :required="true" minlength="8"/>
-                <x-input id="password" class="block mt-1 w-full"
+                <x-input :label="___('password')" name="password" class="block mt-1 w-full"
                                 type="password"
-                                name="password"
                                 required autocomplete="new-password" />
             </div>
 
             <!-- Confirm Password -->
             <div class="mt-4">
-                <x-label for="password_confirmation" :value="___('confirm password')" :required="true" minlength="8"/>
-                <x-input id="password_confirmation" class="block mt-1 w-full"
+                <x-input :label="___('confirm password')" name="password_confirmation"
                                 type="password"
-                                name="password_confirmation" required />
+																required />
             </div>
             
             <!-- Birth date -->
             <div class="mt-4">
-                <x-label for="birthdate" :value="___('birthdate')"/>
-                <x-input id="birthdate" class="block mt-1 w-full" type="date" name="birthdate" :value="old('birthdate')" />
+                <x-input :label="___('birthdate')" name="birthdate" class="block mt-1 w-full" type="date" name="birthdate" :value="old('birthdate')" />
             </div>
 
             <div class="flex items-center justify-end mt-4">
@@ -64,9 +56,7 @@
                     {{ __('Already registered?') }}
                 </a>
 
-                <x-button class="ml-4">
-                    {{ ___('register') }}
-                </x-button>
+                <x-post :href="route('register')" :label="___('register')" />
             </div>
         </form>
     </x-auth-card>
